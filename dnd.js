584a92a509f73e33d022d57003c8c2d6dadb3497
Josua Krause <josua.krause@gmail.com>
2014-06-08 00:54:08 -0400

    correct handling when using same target multiple times

--- dnd.js
+++ dnd.js
@@ -2,27 +2,30 @@
 function initDnD() {
     var targetCount = 0;
     var dragObj = null;
+    var dragSel = null;
     var dragListener = d3.behavior.drag().on("dragstart", function(d) {
         dragObj = d;
+        dragSel = d3.select(this);
         var src = getSource(dragObj);
         src.getGhost().show(dragObj, src);
-        src.dragstart(dragObj);
+        src.dragstart(dragSel, dragObj);
         d3.event.sourceEvent.stopPropagation();
     }).on("drag", function() {
         var src = getSource(dragObj);
-        src.getGhost().move(d3.event.dx, d3.event.dy, dragObj, src);
+        src.getGhost().move(d3.event.dx, d3.event.dy, dragSel, dragObj, src);
         src.drag(dragObj, src);
     }).on("dragend", function() {
         var d = dragObj;
         var t = curTarget;
+        var s = dragSel;
         dragObj = null;
         curTarget = null;
         var src = getSource(d);
         var g = src.getGhost();
         g.hide(d, src);
-        src.dragend(d, src);
+        src.dragend(s, d, src);
         if(t !== null) {
-            t.callDrop(g.gi, d, src);
+            t.callDrop(g.gi, s, d, src);
         }
     });
 
@@ -62,39 +65,51 @@ function initDnD() {
 
     var curTarget = null;
     var curTargetObj = null;
+    var curTargetSel = null;
     function createTarget(source) {
         var id = targetCount++;
         var that = {
             id: function() { return id; },
             register: function(selection, hover, leave, drop) {
-                selection.on("mouseover", function() {
+                var mor = selection.on("mouseover");
+                var mot = selection.on("mouseout");
+                selection.on("mouseover", function(d, i) {
+                    if(mor !== undefined) {
+                        mor.bind(this)(d, i);
+                    }
                     if(dragObj === null) return;
                     var src = getSource(dragObj);
                     if(!src.isTarget(that)) return;
-                    hover(this, dragObj, src);
+                    curTargetSel = d3.select(this);
+                    hover(curTargetSel, dragObj, src);
                     curTarget = that;
                     curTargetObj = this;
                     if(curTargetObj.__drop_target === undefined) {
                         curTargetObj.__drop_target = {};
                     }
-                    curTargetObj.__drop_target[id] = function(g, dragObj, src) {
-                        drop(g, dragObj, src, curTargetObj, that);
-                        leave(curTargetObj, dragObj, src);
+                    curTargetObj.__drop_target[id] = function(g, dragSel, dragObj, src) {
+                        drop(g, dragSel, dragObj, src, curTargetSel, curTargetObj, that);
+                        leave(curTargetSel, curTargetObj, dragObj, src);
                     };
-                }).on("mouseout", function() {
+                }).on("mouseout", function(d, i) {
+                    if(mot !== undefined) {
+                        mot.bind(this)(d, i);
+                    }
                     if(dragObj === null) return;
                     var src = getSource(dragObj);
                     if(!src.isTarget(that)) return;
                     if(curTarget !== that) return;
                     if(curTargetObj !== this) return;
-                    leave(this, dragObj, src);
+                    leave(curTargetSel, curTargetObj, dragObj, src);
                     curTarget = null;
                     curTargetObj = null;
+                    curTargetSel = null;
                 });
             },
-            callDrop: function(g, dragObj, src) {
-                curTargetObj.__drop_target[id](g, dragObj, src);
+            callDrop: function(g, dragSel, dragObj, src) {
+                curTargetObj.__drop_target[id](g, dragSel, dragObj, src);
                 curTargetObj = null;
+                curTargetSel = null;
             }
         };
         source.addTarget(that);
